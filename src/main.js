8b3817095b941da179a6561000b26d86694f2063
gilliantuerah <gilliantuerah@gmail.com>
2020-08-31 18:29:12 +0700

    update js

--- src/main.js
+++ src/main.js
@@ -1,28 +1,29 @@
 // Navigation scroll style
-// window.addEventListener('scroll', event => {
-//     let nav = document.querySelector(' .top-nav');
-//     (window.scrollY >= 44) ? nav.classList.add('scroll') : nav.classList.remove('scroll');
-// })
 window.addEventListener('scroll', event => {
-    let navlinks = document.querySelectorAll('nav ul li a');
-    let top = window.scrollY;
-
-    navlinks.forEach(link => {
-        let sect = document.querySelector(link.hash);
-
-        if (
-            sect.offsetTop <= top && sect.offsetTop + sect.offsetHeight > top
-        ) {
-            link.classList.add('active');
-        } else {
-            link.classList.remove('active');
-        }
-    })
-
-
+    let nav = document.querySelector('.top-nav');
+    (window.scrollY >= 44) ? nav.classList.add('scroll') : nav.classList.remove('scroll');
 })
 
 
+// develop nanti
+// window.addEventListener('scroll', event => {
+//     let navlinks = document.querySelectorAll('nav ul li a');
+//     let top = window.scrollY;
+
+//     navlinks.forEach(link => {
+//         let sect = document.querySelector(link.hash);
+
+//         if (
+//             sect.offsetTop <= top && sect.offsetTop + sect.offsetHeight > top
+//         ) {
+//             link.classList.add('active');
+//         } else {
+//             link.classList.remove('active');
+//         }
+//     })
+// })
+
+// develop nanti
 // message
 // let sendbttn = document.getElementById('btn');
 // let overlay = document.getElementById('overlay');
