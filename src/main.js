082a03a4f7221499f187870fa02249432b189a59
gilliantuerah <gilliantuerah@gmail.com>
2020-08-31 18:13:22 +0700

    update js

--- src/main.js
+++ src/main.js
@@ -1,10 +1,33 @@
-$(document).ready(function () {
-    $('.top-nav .top-link').on('Click', function () {
-        $('.top-nav').removeClass('open');
-    });
-    $('nav a[href*="#"]').on('Click', function () {
-        $('html,body').animate(Keyframes: {
-            scrollTop: $($(this).attr('href')).offset().top - 100
-        }, options: 2000);
-    });
-});
+// Navigation scroll style
+// window.addEventListener('scroll', event => {
+//     let nav = document.querySelector(' .top-nav');
+//     (window.scrollY >= 44) ? nav.classList.add('scroll') : nav.classList.remove('scroll');
+// })
+window.addEventListener('scroll', event => {
+    let navlinks = document.querySelectorAll('nav ul li a');
+    let top = window.scrollY;
+
+    navlinks.forEach(link => {
+        let sect = document.querySelector(link.hash);
+
+        if (
+            sect.offsetTop <= top && sect.offsetTop + sect.offsetHeight > top
+        ) {
+            link.classList.add('active');
+        } else {
+            link.classList.remove('active');
+        }
+    })
+
+
+})
+
+
+// message
+// let sendbttn = document.getElementById('btn');
+// let overlay = document.getElementById('overlay');
+// sendbttn.addEventListener('click', () => {
+//     overlay.style.display = 'grid';
+//     overlay.classList.add('animation-overlay');
+//     console.log("hai");
+// })
